1403c8299d5eb6336c0d7ca61130d501cf774f08
RobinBhandari <Robin.Bhandari@sky.com>
2017-03-22 17:39:16 +0000

    Get page to use angular

--- TaskManager.Website/systemjs.config.js
+++ TaskManager.Website/systemjs.config.js
@@ -1,12 +1,16 @@
-﻿
+﻿/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
 (function (global) {
     System.config({
-        // short-cuts to paths of where packages are
         paths: {
+            // paths serve as alias
             'npm:': 'node_modules/'
         },
-        // taking the shortcut and creating specific paths for modules
+        // map tells the System loader where to look for things
         map: {
+            // our app is within the app folder
             app: 'app',
 
             // angular bundles
@@ -17,17 +21,22 @@
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js'
+            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+            // other libraries
+            'rxjs': 'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+            'ng2-charts': 'npm:ng2-charts/bundles/ng2-charts.umd.js'
         },
-        // for a given package, conventions around the filename
-        // eg. load angular from here .. 
+        // packages tells the System loader how to load when no filename and/or no extension
         packages: {
             app: {
                 main: './main.js',
                 defaultExtension: 'js'
+            },
+            rxjs: {
+                defaultExtension: 'js'
             }
-
-
         }
-    })
-})
+    });
+})(this); 
