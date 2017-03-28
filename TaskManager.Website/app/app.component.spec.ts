62c505356ba2d5e84d2b7676276c197cc44922c2
RobinBhandari <Robin.Bhandari@sky.com>
2017-03-28 15:14:20 +0100

    Add first client side test

---
+++ TaskManager.Website/app/app.component.spec.ts
@@ -0,0 +1,12 @@
+﻿import { AppComponent } from './app.component'
+
+describe('AppComponent', function () {
+
+    it('smoke test', function () {
+        // just testing that karma and jasmine are working
+        expect('1').toEqual('2');
+    });
+
+
+
+});
--- TaskManager.Website/app/main.ts
+++ TaskManager.Website/app/main.ts
@@ -1,4 +1,7 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+﻿
+/// <reference path="../typings/globals/core-js/index.d.ts" /> 
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule); 
